9d28a761423194a28ffb65efa60bd3f0a93265cd
ma321b <aa.muhammad1030@gmail.com>
2021-03-04 17:18:22 +0500

    Add main working code for the frontend

--- frontend/plain-js-frontend/index.js
+++ frontend/plain-js-frontend/index.js
@@ -1,3 +1,6 @@
+
+// TODO: Get starting index of the processes (Like 0 or 1 for P0, P1 respectively)
+
 function onGoClicked() {
     const procNum = document.getElementById("procNum").value;
     const resNum = document.getElementById("resNum").value;
@@ -15,32 +18,35 @@ function renderInputMatrix(procNum, resNum) {
     const procs = parseInt(procNum);
     const res = parseInt(resNum);
     let table = `<table><th>Process</th><th colspan=${res}>Allocation</th><th colspan=${res}>Maximum</th>`;
-    let firstRow = true;
-    for (let i = 0; i <= procs; i++) {
-        let proc_id = i - 1;
+    table += "<tr><td></td>";
+    for (let i = 0; i < 2; i++) {
+        for (let j = 0; j < resNum; j++) {
+            table += `<td style="text-align:right">${alphabets[j]}</td>`;
+        }
+    }
+    table += "</tr>";
+    
+    for (let i = 0; i < procs; i++) {
+        table += `<tr><td>P${i}</td>`;
         for (let j = 0; j < 2; j++) {
             // j for MAX and ALLOC columns
-            table += "<tr>";
             for (let k = 0; k < resNum; k++) {
                 // k for each resource type
-                if (firstRow) {
-                    if (j === 0) {
-                        table += "<td></td>";
-                    } else {
-                        table += `<td>${alphabets[k]}</td>`
-                    }
-                } else {
-                    if (j === 1) {
-                        table += `<td><input type="text" name="" onkeyup="moveToNextRow()"></td>`
-                    } else if (j === 2) {
-
-                    }
+                if (j === 0) {
+                    // the name of the form is alloc_processNum_resourceTypeNum
+                    table += `<td><input type="text" size="2" name="alloc_${i}_${k}" onkeyup="moveToNextRow()"></td>`;
+                } else if (j === 1) {
+                    // the name of the form is maxm_processNum_resourceTypeNum
+                    table += `<td><input type="text" size="2" name="maxm_${i}_${k}" onkeyup="moveToNextRow()"></td>`;
                 }
             }
-            table += "</tr>";
         }
-        firstRow = false;
+        table += "</tr>";
     }
+    table += "</table>\n\n\n";
+    let button = `<button onClick="onCalcClicked()">Calculate</button>`;
+    table += button;
+    document.getElementById("main-pg").innerHTML = table;
 }
 
 // move cursor to the next form field (handling onkeyup event)
@@ -50,5 +56,10 @@ function moveToNextRow(given, maxm) {
 
 // returns the form to get the data field
 function renderAvailable(procNum, resNum) {
+
+}
+
+// send data to server and render the response 
+function onCalcClicked() {
 
 }
